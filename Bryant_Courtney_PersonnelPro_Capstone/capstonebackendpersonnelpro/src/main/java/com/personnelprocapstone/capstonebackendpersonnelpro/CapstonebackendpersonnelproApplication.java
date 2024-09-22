8b98c1773ef8eaa252b259ac04e2ad215c475887
Mscourtneybryant <87495361+Mscourtneybryant@users.noreply.github.com>
2024-09-22 17:07:06 -0500

    added a new backend

---
+++ Bryant_Courtney_PersonnelPro_Capstone/capstonebackendpersonnelpro/src/main/java/com/personnelprocapstone/capstonebackendpersonnelpro/CapstonebackendpersonnelproApplication.java
@@ -0,0 +1,13 @@
+package com.personnelprocapstone.capstonebackendpersonnelpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstonebackendpersonnelproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstonebackendpersonnelproApplication.class, args);
+	}
+
+}
---
+++ Bryant_Courtney_PersonnelPro_Capstone/capstonebackendpersonnelpro/src/test/java/com/personnelprocapstone/capstonebackendpersonnelpro/CapstonebackendpersonnelproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personnelprocapstone.capstonebackendpersonnelpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstonebackendpersonnelproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
